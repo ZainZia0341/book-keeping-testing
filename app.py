6b8182d03dd59efcb2ac9f2e285cca46ebde5440
Zain Zia <zainzia0341@gmail.com>
2024-12-31 18:29:10 +0500

    new improve prompts

--- app.py
+++ app.py
@@ -803,7 +803,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3004"],  # Frontend origin
+    allow_origins=["*"],  # Frontend origin
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
